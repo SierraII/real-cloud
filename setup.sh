6c25e3e3f71daa86580e3f0c6fbeede1da5bf4ce
Adrian David Smith <sierra.as74@gmail.com>
2017-07-19 12:40:40 +0200

    Webpack and Base

---
+++ setup.sh
@@ -0,0 +1,7 @@
+npm install
+
+npm install -g babel
+
+npm install -g babel-cli
+
+npm install webpack --save
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+var config = {
+
+   entry: './src/frontend/js/main.js',
+
+   output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'main.bundle.js'
+    },
+
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+
+   resolveLoader: {
+       moduleExtensions: ['-loader']
+    },
+
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config;
