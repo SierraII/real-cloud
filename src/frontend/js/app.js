05161cca8397209e8dde44497c1bc9293e6787d2
Adrian David Smith <sierra.as74@gmail.com>
2017-07-20 08:36:45 +0200

    Fixed Broken Dependencies

--- src/frontend/js/app.js
+++ src/frontend/js/app.js
@@ -1,66 +1,84 @@
 import React from 'react';
 import axios from 'axios';
-import CandleStickStockScaleChart from './CandleStickStockScaleChart.js';
 
-// https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=BEDQE0RSQK7E37S6
+import CandleStickStockScaleChart from './CandleStickStockScaleChart.js';
 
 class App extends React.Component{
 
-    state : {
+    constructor(props){
 
-    };
+        super(props);
 
-    render(){
+        this.state = {
+            data: null
+        };
+
+    }
+
+    componentDidMount(){
+
+        // var parseDate = d3.timeParse("%Y-%m-%d %H:%M:%S");
+        //
+        // d3["tsv"]("//rrag.github.io/react-stockcharts/data/MSFT.tsv", (err, data) => {
+        //
+        // 	data.forEach((d, i) => {
+        // 		d.date = new Date(d3.timeParse("%Y-%m-%d")(d.date).getTime());
+        // 		d.open = +d.open;
+        // 		d.high = +d.high;
+        // 		d.low = +d.low;
+        // 		d.close = +d.close;
+        // 		d.volume = +d.volume;
+        //         // console.log(d);
+        // 	});
+        //
+        // });
 
         axios.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=BEDQE0RSQK7E37S6").then(result => {
-           console.log(result);
+        //    console.log(result.data);
         });
 
         var data = [
             {
-                "date": new Date(),
-                "open": 5705.45,
-                "high": 5716.6,
-                "low": 5496.05,
-                "close": 5507.85
+                "date": new Date(2012, 0, 1),
+                "open": 57.08436360087473,
+                "high": 57.451879819431255,
+                "low": 57.451879819431255,
+                "close": 57.143959
             },
             {
-                "date": new Date(),
-                "open": 5705.45,
-                "high": 5716.6,
-                "low": 5496.05,
-                "close": 5507.85
-            },
-            {
-                "date": new Date(),
-                "open": 5705.45,
-                "high": 5716.6,
-                "low": 5496.05,
-                "close": 5507.85
-            },
-            {
-                "date": new Date(),
-                "open": 5705.45,
-                "high": 5716.6,
-                "low": 5496.05,
-                "close": 5507.85
+                "date": new Date(2012, 6, 1),
+                "open": 57.08436360087473,
+                "high": 57.451879819431255,
+                "low": 57.451879819431255,
+                "close": 57.143959
             }
         ];
 
-        d3.request("./frontend/js/CandleStickStockScaleChart.js")
-          .get(function(err, data) {
-            var outputEl = document.getElementById('chart');
-            try {
-              var output = Babel.transform(data.responseText, { presets: ["es2015", "react", "stage-3"] }).code;
-              eval(output);
-            } catch (ex) {
-              outputEl.innerHTML = 'ERROR: ' + ex.message;
-            }
-        });
+        this.setState({data: data});
 
-        return(
-            <div id="chart"></div>
-        );
+    }
+
+    render(){
+
+
+
+        var type = "svg";
+
+        if (this.state.data){
+
+            return(
+                <div id="chart">
+                    <CandleStickStockScaleChart data={this.state.data} type={type} />
+                </div>
+            );
+
+        }
+
+        else{
+            return(<div><h1>loading...</h1></div>);
+        }
+
+
 
     }
 
