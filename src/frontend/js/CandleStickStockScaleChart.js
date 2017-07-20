9cfd021f0d47c7e6d98be431358f1597d93297ce
Adrian David Smith <sierra.as74@gmail.com>
2017-07-20 10:39:56 +0200

    Chart UI

--- src/frontend/js/CandleStickStockScaleChart.js
+++ src/frontend/js/CandleStickStockScaleChart.js
@@ -2,9 +2,11 @@
 
 import React from 'react';
 import axios from 'axios';
-import { ChartCanvas, Chart, series, scale, coordinates, tooltip, axes, indicator, helper } from "react-stockcharts";
+
+import { ChartCanvas, Chart, series, scale, coordinates, annotation, tooltip, axes, indicator, helper } from "react-stockcharts";
 
 var { CrossHairCursor, MouseCoordinateX, MouseCoordinateY } = coordinates;
+var { Annotate, LabelAnnotation, Label } = annotation;
 var { OHLCTooltip } = tooltip;
 
 var { discontinuousTimeScaleProvider } = scale;
@@ -34,6 +36,7 @@ class CandleStickStockScaleChart extends React.Component{
 
     componentDidMount(){
 
+        // get the data
         var data = [];
 
         axios.get("https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=" + this.props.symbol + "&interval=1min&apikey=BEDQE0RSQK7E37S6").then(result => {
@@ -69,7 +72,17 @@ class CandleStickStockScaleChart extends React.Component{
 
 	render(){
 
-		var { data, width, ratio } = this.props;
+		var { width, ratio } = this.props;
+
+        var margin = {left: 20, right: 80, top:130, bottom: 250};
+        var height = 750;
+
+        var gridHeight = height - margin.top - margin.bottom;
+        var gridWidth = width - margin.left - margin.right;
+
+        var showGrid = true;
+        var yGrid = showGrid ? { innerTickSize: -1 * gridWidth, tickStrokeOpacity: 0.2 } : {};
+        var xGrid = showGrid ? { innerTickSize: -1 * gridHeight, tickStrokeOpacity: 0.2 } : {};
 
         if (this.state.data){
 
@@ -82,15 +95,20 @@ class CandleStickStockScaleChart extends React.Component{
     					xAccessor={d => d.date} xScaleProvider={discontinuousTimeScaleProvider}
     					xExtents={[new Date(2001, 0, 1), new Date(2017, 6, 2)]}>
 
+                        <Label x={(width - margin.left - margin.right) / 2} y={(height - margin.top - margin.bottom) / 2}
+                        fontSize="40" text={this.props.symbol} fill="#FFFFFF" opacity="0.2"/>
+
     				<Chart id={1} yExtents={d => [d.high, d.low]}>
-    					<XAxis axisAt="bottom" orient="bottom" ticks={6}/>
-    					<YAxis axisAt="left" orient="left" ticks={5} />
-    					<CandlestickSeries />
+
+                        <YAxis axisAt="left" orient="left" ticks={5} {...yGrid} inverted={true} tickStroke="#FFFFFF" />
+                        <XAxis axisAt="bottom" orient="bottom" ticks={5} {...xGrid} inverted={true} tickStroke="#FFFFFF"/>
+
+        				<CandlestickSeries wickStroke={d => d.close > d.open ? "#505667" : "#505667"}
+							fill={d => d.close > d.open ? "#26ff00" : "#ff1500"}/>
     				</Chart>
 
-                    <OHLCTooltip forChart={1} origin={[-40, 0]}/>
-
-                    <CrossHairCursor />
+                    <OHLCTooltip forChart={1} origin={[20, 350]}/>
+                    <CrossHairCursor stroke="#FFFFFF"/>
 
     			</ChartCanvas>
 
