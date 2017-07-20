614373693e203edc5efe6a2e715d69bd8931cffc
Adrian David Smith <sierra.as74@gmail.com>
2017-07-20 08:00:23 +0200

    Base Chart

--- src/frontend/js/app.js
+++ src/frontend/js/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-
-var LineChart = require("react-chartjs").Bar;
+import CandleStickStockScaleChart from './CandleStickStockScaleChart.js';
 
 // https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=BEDQE0RSQK7E37S6
 
@@ -14,16 +13,53 @@ class App extends React.Component{
     render(){
 
         axios.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=BEDQE0RSQK7E37S6").then(result => {
-           console.log(result.data["Time Series (1min)"]);
+           console.log(result);
+        });
 
+        var data = [
+            {
+                "date": new Date(),
+                "open": 5705.45,
+                "high": 5716.6,
+                "low": 5496.05,
+                "close": 5507.85
+            },
+            {
+                "date": new Date(),
+                "open": 5705.45,
+                "high": 5716.6,
+                "low": 5496.05,
+                "close": 5507.85
+            },
+            {
+                "date": new Date(),
+                "open": 5705.45,
+                "high": 5716.6,
+                "low": 5496.05,
+                "close": 5507.85
+            },
+            {
+                "date": new Date(),
+                "open": 5705.45,
+                "high": 5716.6,
+                "low": 5496.05,
+                "close": 5507.85
+            }
+        ];
+
+        d3.request("./frontend/js/CandleStickStockScaleChart.js")
+          .get(function(err, data) {
+            var outputEl = document.getElementById('chart');
+            try {
+              var output = Babel.transform(data.responseText, { presets: ["es2015", "react", "stage-3"] }).code;
+              eval(output);
+            } catch (ex) {
+              outputEl.innerHTML = 'ERROR: ' + ex.message;
+            }
         });
 
         return(
-
-            <div>
-                wtf
-            </div>
-
+            <div id="chart"></div>
         );
 
     }
