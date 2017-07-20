5df7ddcf5e16ac565f1d895bd78c11608ff958a3
Adrian David Smith <sierra.as74@gmail.com>
2017-07-20 09:40:32 +0200

    Mouse Events

--- src/frontend/js/CandleStickStockScaleChart.js
+++ src/frontend/js/CandleStickStockScaleChart.js
@@ -1,7 +1,11 @@
 'use strict';
 
 import React from 'react';
+import axios from 'axios';
 import { ChartCanvas, Chart, series, scale, coordinates, tooltip, axes, indicator, helper } from "react-stockcharts";
+
+var { CrossHairCursor, MouseCoordinateX, MouseCoordinateY } = coordinates;
+var { OHLCTooltip } = tooltip;
 
 var { discontinuousTimeScaleProvider } = scale;
 var { CandlestickSeries } = series;
@@ -10,45 +14,97 @@ var { fitWidth } = helper;
 
 class CandleStickStockScaleChart extends React.Component{
 
+    constructor(props){
+
+        super(props);
+
+        this.state = {
+            data: null
+        };
+
+    }
+
+    propTypes: {
+
+    	width: React.PropTypes.number.isRequired,
+    	ratio: React.PropTypes.number.isRequired,
+        symbol: React.PropTypes.string.isRequired
+
+    }
+
+    componentDidMount(){
+
+        var data = [];
+
+        axios.get("https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=" + this.props.symbol + "&interval=1min&apikey=BEDQE0RSQK7E37S6").then(result => {
+
+            var monthlySeries = result.data["Monthly Time Series"];
+
+            for(var key in monthlySeries){
+
+                if(monthlySeries.hasOwnProperty(key)){
+
+                    var value = monthlySeries[key];
+                    var month = {};
+
+                    month["date"] = new Date(key);
+
+                    month["open"] = parseFloat(value["1. open"]);
+                    month["high"] = parseFloat(value["2. high"]);
+                    month["low"] = parseFloat(value["3. low"]);
+                    month["close"] = parseFloat(value["4. close"]);
+                    month["volume"] = parseFloat(value["5. volume"]);
+
+                    data.unshift(month);
+
+                }
+
+            }
+
+            this.setState({data: data});
+
+        });
+
+    }
+
 	render(){
 
-		var { type, data, width, ratio } = this.props;
+		var { data, width, ratio } = this.props;
 
-		return(
+        if (this.state.data){
 
-			<ChartCanvas ratio={ratio} width={width} height={400}
-					margin={{ left: 50, right: 50, top: 10, bottom: 30 }} type={type}
-					seriesName="MSFT"
-					data={data}
-					xAccessor={d => d.date} xScaleProvider={discontinuousTimeScaleProvider}
-					xExtents={[new Date(2001, 0, 1), new Date(2017, 6, 2)]}>
+            return(
 
-				<Chart id={1} yExtents={d => [d.high, d.low]}>
-					<XAxis axisAt="bottom" orient="bottom" ticks={6}/>
-					<YAxis axisAt="left" orient="left" ticks={5} />
-					<CandlestickSeries />
-				</Chart>
+                <ChartCanvas ratio={ratio} width={width} height={400}
+    					margin={{ left: 50, right: 50, top: 10, bottom: 30 }} type="svg"
+    					seriesName="MSFT"
+    					data={this.state.data}
+    					xAccessor={d => d.date} xScaleProvider={discontinuousTimeScaleProvider}
+    					xExtents={[new Date(2001, 0, 1), new Date(2017, 6, 2)]}>
 
-			</ChartCanvas>
+    				<Chart id={1} yExtents={d => [d.high, d.low]}>
+    					<XAxis axisAt="bottom" orient="bottom" ticks={6}/>
+    					<YAxis axisAt="left" orient="left" ticks={5} />
+    					<CandlestickSeries />
+    				</Chart>
 
-		);
+                    <OHLCTooltip forChart={1} origin={[-40, 0]}/>
+
+                    <CrossHairCursor />
+
+    			</ChartCanvas>
+
+            );
+
+        }
+
+        else{
+            return(<div><h1>loading...</h1></div>);
+        }
 
 	}
 
 }
-
-CandleStickStockScaleChart.propTypes = {
-
-	data: React.PropTypes.array.isRequired,
-	width: React.PropTypes.number.isRequired,
-	ratio: React.PropTypes.number.isRequired,
-	type: React.PropTypes.oneOf(["svg", "hybrid"]).isRequired
-
-};
-
-CandleStickStockScaleChart.defaultProps = {
-	type: "svg"
-};
 
 CandleStickStockScaleChart = fitWidth(CandleStickStockScaleChart);
 
--- src/frontend/js/app.js
+++ src/frontend/js/app.js
@@ -1,74 +1,17 @@
 import React from 'react';
-import axios from 'axios';
-
 import CandleStickStockScaleChart from './CandleStickStockScaleChart.js';
 
 class App extends React.Component{
 
-    constructor(props){
-
-        super(props);
-
-        this.state = {
-            data: null
-        };
-
-    }
-
-    componentDidMount(){
-
-        var data = [];
-
-        axios.get("https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=MSFT&interval=1min&apikey=BEDQE0RSQK7E37S6").then(result => {
-
-            var monthlySeries = result.data["Monthly Time Series"];
-
-            for(var key in monthlySeries){
-
-                if(monthlySeries.hasOwnProperty(key)){
-
-                    var value = monthlySeries[key];
-                    var month = {};
-
-                    month["date"] = new Date(key);
-
-                    month["open"] = parseFloat(value["1. open"]);
-                    month["high"] = parseFloat(value["2. high"]);
-                    month["low"] = parseFloat(value["3. low"]);
-                    month["close"] = parseFloat(value["4. close"]);
-                    month["volume"] = parseFloat(value["5. volume"]);
-
-                    console.log(month);
-
-                    data.unshift(month);
-
-                }
-
-            }
-
-            this.setState({data: data});
-
-        });
-
-    }
-
     render(){
 
-        var type = "svg";
+        return(
 
-        if (this.state.data){
+            <div id="chart-container">
+                <CandleStickStockScaleChart symbol="GOOGL"/>
+            </div>
 
-            return(
-                <div id="chart">
-                    <CandleStickStockScaleChart data={this.state.data} type={type}/>
-                </div>
-            );
-
-        }
-
-        else{
-            return(<div><h1>loading...</h1></div>);
-        }
+        );
 
     }
 
