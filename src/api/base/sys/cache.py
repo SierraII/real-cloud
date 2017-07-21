4f507d86b1e39f0d90b8b81be5e5e5d791bb39d5
Adrian David Smith <sierra.as74@gmail.com>
2017-07-21 14:04:03 +0200

    cache

--- src/api/base/sys/cache.py
+++ src/api/base/sys/cache.py
@@ -1,2 +1,10 @@
-def test():
-    print "test"
+from werkzeug.contrib.cache import SimpleCache
+
+SimpleCache = SimpleCache()
+
+def set(key, data, expire_time_in_minutes):
+    SimpleCache.set(key, data, timeout= expire_time_in_minutes * 60)
+
+
+def get(key):
+    SimpleCache.get(key)
--- src/api/core.py
+++ src/api/core.py
@@ -1,15 +1,14 @@
 import requests
 import os
 
-from utils import lib
+from utils import tools
+from base.sys import cache
 
-from werkzeug.contrib.cache import SimpleCache
 from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
-cache = SimpleCache()
 
 @app.route('/news/get', methods=['GET'])
 def get_news():
@@ -19,13 +18,13 @@ def get_news():
 
     if news is None:
         news = requests.get('https://www.google.co.uk/finance/company_news?output=rss&q=NASDAQ:' + symbol).content
-        cache.set(symbol, news, timeout= 5*60)
+        cache.set(symbol, news, 5)
 
     return news
 
 
 if __name__ == '__main__':
 
-    watch_files = lib.get_directory_files(['./src/api/']);
+    watch_files = tools.get_directory_files(['./src/api/']);
 
     app.run(debug=True, threaded=True, extra_files=watch_files)
